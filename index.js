6039b4465f2eea50865e16b4ebfcc4134e87ce4d
Sergey Sherstobitov <in43sh@gmail.com>
2022-02-02 03:28:41 -0800

    Remove some code that is not needed now.

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ const app = express();
 // app.set('port', process.env.PORT || 3000)
 const port = process.env.PORT || 3000;
 
-// Static files
-app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"));
-app.use("/js", express.static(__dirname + "public/js"));
-app.use("/img", express.static(__dirname + "public/img"));
-
-// Set views
-app.set("views", "./views");
-app.set("view engine", "ejs");
-
 // Set Routes
 app.get("", (req, res) => {
   res.render("index", { text: "This is EJS" });
