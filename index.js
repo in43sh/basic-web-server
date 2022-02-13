42589ff865e3b2c34d19b2009b1c4bf7a03c8bba
Sergey Sherstobitov <in43sh@gmail.com>
2022-02-12 20:21:02 -0800

    Fix the bug with routes not working.

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-// Import
+// import
 const express = require("express");
 const app = express();
 // const port = 3000;
 // app.set('port', process.env.PORT || 3000)
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./public/views'));
 
-// Set Routes
+// set routes
 // send static file as response
 app.get("/", (req, res) => {
-  res.render("", { text: "This is EJS" });
+  // res.render("", { text: "This is EJS" });
   // res.type('text/html');
-  // res.sendFile('/index.html');
+  res.sendFile('./public/views/index.html', { root: __dirname });
 });
 
 // send plain text response
 app.get("/about", (req, res) => {
   // res.render("index", { text: "This is EJS" });
-  res.type('text/plain');
-  res.send('About page');
+  // res.type('text/plain');
+  res.sendFile('./public/views/about.html', { root: __dirname });
 });
 
 // define 404 handler
